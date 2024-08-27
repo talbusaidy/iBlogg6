13ddd36d1db5aadf79489f9a210877254ac4edee
talbusaidy <talibalbusaidy1130@gmail.com>
2024-08-27 18:54:12 +0300

    initial commit

---
+++ app/src/main/java/com/example/iblogg/Topics/AddNewTopic.kt
@@ -0,0 +1,17 @@
+package com.example.iblogg.Topics
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AddNewTopic(navController: NavController){
+
+}
+
+
+
+@Composable
+fun AddNewTopicPrev(){
+    AddNewTopic(rememberNavController())
+}
